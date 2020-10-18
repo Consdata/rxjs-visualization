9a5feeb9043a15e12b9c82f2b680ddf18b51e5cb
Konrad Gabara <gabara.konrad@gmail.com>
2020-10-18 16:24:35 +0200

    initial commit

---
+++ apps/rxjs-presentation/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arges-root',
+  template: `
+    <router-outlet></router-outlet>`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rxjs-presentation';
+}
